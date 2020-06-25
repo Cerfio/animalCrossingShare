c8dff737d92bf33f2fb46d68aa62dde1e864a3cd
Cerfio <cerfio>
2020-06-25 12:36:53 +0200

    [Remove] remove code, component to update later

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,31 @@
 export default function Header() {
-  return <h1 className="title">Welcome to my app!</h1>
+  return (
+    <div className="container">
+    <style jsx>{`
+      .container {
+        height: 20vh;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        align-items: center;
+      }
+
+      main {
+        padding: 5rem 0;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+      }
+
+      code {
+        background: #fafafa;
+        border-radius: 5px;
+        padding: 0.75rem;
+        font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;
+      }
+  `}</style>
+  </div>
+  )
 }
