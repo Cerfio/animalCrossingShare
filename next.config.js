790e71753f8fec39b746778686a5da52fb99874f
Cerfio <cerfio>
2020-06-26 21:44:25 +0200

    [ADD] Antdesign component ui

--- next.config.js
+++ next.config.js
@@ -1,2 +1,18 @@
-const withImages = require('next-images')
+const withImages = require('next-images');
+const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass')
+const withLess = require('@zeit/next-less')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+if (typeof require !== 'undefined') {
+  require.extensions['.less'] = file => { }
+}
+
+
 module.exports = withImages({})
+module.exports = withLess(withSass({
+  lessLoaderOptions: {
+    javascriptEnabled: true
+  }
+}))
