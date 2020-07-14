b387512471e87a3efaef81e2a85f0a80c0ebd8a2
Cerfio <cerfio>
2020-07-14 21:39:17 +0200

    [UPDATE] norm Card component

--- components/Card.js
+++ components/Card.js
@@ -1,20 +1,19 @@
 export default function Card(props) {
     return (
-      <div className="card">
-        <img className="card__image" src={props.image}></img>
-        <p>{props.title ? props.title : "defaultTitle"}</p>
-        <p>by {props.author ? props.author : "unknow"}</p>
-        <style jsx>{`
-          .card {
-
-          }
-          .card__image {
-            object-fit: cover;
-            width: 200px;
-            height: 200px;
-          }
-        `}</style>
-      </div>
+    	<div className="card">
+        	<img className="card__image" src={props.image}></img>
+        	<p>{props.title ? props.title : "defaultTitle"}</p>
+        	<p>by {props.author ? props.author : "unknow"}</p>
+        	<style jsx>{`
+          		.card {
+          		}
+          		.card__image {
+            		object-fit: cover;
+            		cursor: pointer;
+            		width: 200px;
+            		height: 200px;
+          		}
+        	`}</style>
+      	</div>
     )
-  }
-  
+}
