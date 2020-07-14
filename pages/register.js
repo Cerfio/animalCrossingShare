e88f0114d28eb077f15d5664d4751d4f3d63b5ba
Cerfio <cerfio>
2020-07-14 02:30:17 +0200

    [UPDATE] connect register page with back-end

--- pages/register.js
+++ pages/register.js
@@ -1,64 +1,57 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import React from "react";
+import {
+  Box,
+  Button,
+  Grommet,
+  Form,
+  FormField,
+  Text,
+  TextInput
+} from "grommet";
+import { grommet } from "grommet/themes";
+import { message} from 'antd';
+import Router from 'next/router'
+import axios from 'axios';
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const RegisterPage = () => {
+	const registerRequest = async (value) => {
+		try {
+      		const responseCreationUser = await axios.post(process.env.NEXT_PUBLIC_BACK_HOST + '/user/register', value);
 
-      <main>
-        <Header />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+			message.success(responseCreationUser.data);
+      		Router.push('/login');
+    	} catch(e) {
+      		message.error(e.response.data);
+    	}
+  	}
+  	const [value, setValue] = React.useState({ email: '', password: '', pseudo: '' });
+  	return (
+	    <Grommet full theme={grommet}>
+    		<Box fill align="center" justify="center">
+        		<Box width="medium">
+          			<Form
+            			value={value}
+            			onChange={nextValue => setValue(nextValue)}
+            			onSubmit={({ value: nextValue }) => registerRequest(nextValue)}>
+            			<FormField label="Pseudo" name="pseudo" required>
+              				<TextInput name="pseudo" type="text" />
+            			</FormField>
+            			<FormField label="Email" name="email" required>
+              				<TextInput name="email" type="email" />
+            			</FormField>
+            			<FormField label="Password" name="password" required>
+              				<TextInput name="password" type="password" />
+            			</FormField>
+            			<Box direction="row" justify="between" margin={{ top: "medium" }}>
+              				<Button onClick={() => {name = '', email = '', pseudo = ''}} type="reset" label="Reset" />
+              				<Button type="submit" label="Register" primary />
+            			</Box>
+          			</Form>
+          			<p style={{ cursor: 'pointer' }} onClick={() => { Router.push('/login') }}>Already register click here!</p>
+        		</Box>
+      		</Box>
+    	</Grommet>
+  	);
+};
 
-      <Footer />
-
-      <style jsx>{`
-        .container {
-          height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
-}
+export default RegisterPage;
